42eac9bdccba157d886d4487a21b16d30dfe5dea
Hunter Baker <hunter.05baker@gmail.com>
2022-04-13 20:33:23 -0500

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Pokemon from './Pokemon';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
+        <Pokemon />
         <a
           className="App-link"
           href="https://reactjs.org"
