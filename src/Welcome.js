20f7037fcd942b1ec9157513b7cccfef139da466
Hunter Baker <hunter.05baker@gmail.com>
2022-04-15 14:05:11 -0500

    main

---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Welcome to my PokeAPI!</h1>;
+  }
+}
+
+// component goes here:
+<MyComponentClass />
+ReactDOM.render(<MyComponentClass />, document.getElementById('app'));
