2c9f242c7dda6ba487d78fb2081d77cfc5bda362
Hunter Baker <hunter.05baker@gmail.com>
2022-04-18 11:53:03 -0500

    mian

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Pokemon = () => {
     return (
-        <p>Type in a pokemon to get started</p>
+        <><p>Type in a pokemon to get started</p><p>Here are some pokemon below, type in a certain pokemon in the box above to get more in depth detail</p></>
     )
 }
 
