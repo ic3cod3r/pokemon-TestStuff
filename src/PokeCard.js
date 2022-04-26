62ef22db2b066f6ae3c2308f081881ff3bc6fc47
Hunter Baker <hunter.05baker@gmail.com>
2022-04-26 17:58:05 -0500

    added place for abilities and type

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -2,16 +2,17 @@ import React from 'react'
 
 
 const PokeCard = ({pokemon}) => {
-    return (
-        <div className="card text-center mx-auto" style={{"maxWidth" : "18rem"}} key={pokemon.id}>
+    return (        
+        <div className="card text-center mx-auto" style={{ "maxWidth": "18rem" }} key={pokemon.id}>
         <div className="card-header"><a href="https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pokémon)">{pokemon.name}</a></div>
-        <div className="card-body">          
-          <h6 className="card-subtitle mb-2 text-muted">Id: {pokemon.id}</h6>  
-          <h6 className="card-subtitle mb-2 text-muted">Height: {pokemon.height}</h6>  
-          <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>  
+        <div className="card-body">
+          <h6 className="card-subtitle mb-2 text-muted">Id: {pokemon.id}</h6>
+          <h6 className="card-subtitle mb-2 text-muted">Height: {pokemon.height}</h6>
+          <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>
+          <h6 className="card-subtitle mb-2 text-muted">Abilities: {pokemon.ability}</h6>
           <h6 className="card-subtitle mb-2 text-muted">Type: {pokemon.type}</h6>
           <img src={pokemon.sprites['front_default']} />
-          <img src={pokemon.sprites['back_default']} />                       
+          <img src={pokemon.sprites['back_default']} />
         </div>
       </div>
     )
