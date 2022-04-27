914ffd0656aac77075baa289ac0bdf8a2348b114
Hunter Baker <hunter.05baker@gmail.com>
2022-04-27 17:27:41 -0500

    trying to add type to pokemon card

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -8,9 +8,10 @@ const PokeCard = ({pokemon}) => {
         <div className="card-body">
           <h6 className="card-subtitle mb-2 text-muted">Id: {pokemon.id}</h6>
           <h6 className="card-subtitle mb-2 text-muted">Height: {pokemon.height}</h6>
-          <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>
-          <h6 className="card-subtitle mb-2 text-muted">Abilities: {pokemon.ability}</h6>
           <h6 className="card-subtitle mb-2 text-muted">Type: {pokemon.type}</h6>
+          <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>
+         
+          
           <img src={pokemon.sprites['front_default']} />
           <img src={pokemon.sprites['back_default']} />
         </div>
