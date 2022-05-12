8e2ebc02da5117f59e4d5b86ccc8ce949f928839
Hunter Baker <hunter.05baker@gmail.com>
2022-05-12 17:53:48 -0500

    main

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const PokeCard = ({pokemon}) => {
-
+    const [isExpanded, thing2] = useState(false);
+    
     return (
         <div className="col-sm-4" key={pokemon.id} >
             <div className="card text-center mx-auto" >
@@ -11,12 +12,16 @@ const PokeCard = ({pokemon}) => {
                         {pokemon.name}
                     </a>
                 </div>
-                <div className="card-body">
-                    <h6 className="card-subtitle mb-2 text-muted">Id: {pokemon.id}</h6>
-                    <h6 className="card-subtitle mb-2 text-muted">Height: {pokemon.height}</h6>
-                    <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>
-                    <h6 className="card-subtitle mb-2 text-muted">Abilities: {pokemon.ability}</h6>
-                    <h6 className="card-subtitle mb-2 text-muted">Type: {pokemon.type}</h6>
+                <div className="card-body"> 
+                    { isExpanded && (
+                        <>    
+                            <h6 className="card-subtitle mb-2 text-muted">Id: {pokemon.id}</h6>
+                            <h6 className="card-subtitle mb-2 text-muted">Height: {pokemon.height}</h6>
+                            <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>
+                            <h6 className="card-subtitle mb-2 text-muted">Abilities: {pokemon.ability}</h6>
+                            <h6 className="card-subtitle mb-2 text-muted">Type: {pokemon.type}</h6>
+                        </>
+                    )}
                     <img src={pokemon.sprites['front_default']} alt={`Front-facing view of ${pokemon.name}`} />
                     <img src={pokemon.sprites['back_default']} alt={`Back-facing view of ${pokemon.name}`} />
                 </div>
